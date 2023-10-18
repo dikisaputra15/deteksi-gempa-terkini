5e34091143d0842c61965c35b6eb8a509b70b1e8
dikisaputra15 <mbpretina@MBPRetinas-MacBook-Pro.local>
2023-10-19 05:09:50 +0700

    add main file

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+aplikasi deteksi gempa terkini
+"""
+
+def ekstraksi_data():
+    pass
+
+def tampilkan_data(result):
+    pass
+
+if __name__ == '__main__':
+    print ('Aplikasi Utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
